e9cbf110edbede46696e636f9b4775401ee171a9
rey-andre <reynoldsantui@gmail.com>
2023-07-19 19:19:33 +0700

    reset vscode

--- resources/views/example.blade.php
+++ resources/views/example.blade.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Laravel 9</title>
 </head>
 
 <body>
     <p>{{ $students->name }}:</p>
     @foreach ($activities as $activity)
-    <p>{{ $activity->name }}</p>
+        <p>{{ $activity->name }}</p>
     @endforeach
 </body>
 
